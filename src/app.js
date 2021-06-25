619b2b5c09f23aedfe52aa920ee91187b87d018f
delficasarino <delficasarino@gmail.com>
2021-06-25 14:19:08 -0300

    actualizacion json y app.js para heroku

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require ("express");
 const app = express ();
 
-app.listen(3080,() => console.log("Server start", "http://localhost:3080"));
+app.set("port",process.env.PORT || 3080)
+
+app.listen(app.get("port"),() => console.log("Server Start http://localhost:"+app.get("port")))
+
+/* app.listen(3080,() => console.log("Server start", "http://localhost:3080")); */
 
 app.use(express.static("../public")); //.static todo archivo que se quiera exponer de forma publica en el servidor
 
