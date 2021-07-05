74e849b8e9995c7a40a62543ca1c67c88dae4737
delficasarino <delficasarino@gmail.com>
2021-07-05 17:33:24 -0300

    ruteo del header

--- src/routes/web.js
+++ src/routes/web.js
@@ -4,4 +4,11 @@ const path = require("path");
 
 app.get("/", (req,res) => res.sendFile(path.resolve(__dirname,"../views/","index.html")));
 
+app.get("/brunch-afternoon-tea", (req,res) => res.sendFile(path.resolve(__dirname,"../views/","brunch-afternoon-tea.html")));
+app.get("/cataloghi", (req,res) => res.sendFile(path.resolve(__dirname,"../views/","cataloghi.html")));
+app.get("/chi-siamo", (req,res) => res.sendFile(path.resolve(__dirname,"../views/","chi-siamo.html")));
+app.get("/conttato", (req,res) => res.sendFile(path.resolve(__dirname,"../views/","conttato.html")));
+app.get("/eventi", (req,res) => res.sendFile(path.resolve(__dirname,"../views/","eventi.html")));
+app.get("/shop", (req,res) => res.sendFile(path.resolve(__dirname,"../views/","shop.html")));
+
 module.exports = app;
