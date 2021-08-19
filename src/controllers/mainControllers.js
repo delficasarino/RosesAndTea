2bc3ae76e2d0c0b148818d563171494303d91ef3
delficasarino <delficasarino@gmail.com>
2021-08-18 23:57:19 -0300

    routes

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -2,4 +2,22 @@ module.exports = {
     index: (req,res) => {
         return res.render('index', {})
     },
+    chiSiamo: (req,res) => {
+        return res.render('chi-siamo', {})
+    },
+    eventi: (req,res) => {
+        return res.render('eventi', {})
+    },
+    cataloghi: (req,res) => {
+        return res.render('cataloghi', {})
+    },
+    brunchAfternoon: (req,res) => {
+        return res.render('brunch-afternoon-tea', {})
+    },
+    shop: (req,res) => {
+        return res.render('shop', {})
+    },
+    contatto: (req,res) => {
+        return res.render('contatto', {})
+    },
 }
