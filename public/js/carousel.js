79861210cb78a7e65bfbde498922bb7d2ca5bde1
delficasarino <delficasarino@gmail.com>
2021-08-22 20:14:01 -0300

    carousel movil

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -21,6 +21,6 @@ for (i = 0; i < slides.length; i++) {
 for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
 }
-slides[slideIndex-1].style.display = "block";  
+slides[slideIndex-1].style.display = "flex";  
 dots[slideIndex-1].className += " active";
 }
